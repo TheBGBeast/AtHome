94560878ecd4a92cfce7bd0e706af20f8632a57f
vasko <vaskopetrovle009@gmail.com>
2023-03-23 23:05:45 +0200

    1st commit

--- to-do list/to-do list/Form1.Designer.cs
+++ to-do list/to-do list/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.removebtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(1, 0);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(799, 394);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(455, 414);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(107, 23);
+            this.addbtn.TabIndex = 2;
+            this.addbtn.Text = "Add to list";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(681, 414);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(107, 23);
+            this.savebtn.TabIndex = 3;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // removebtn
+            // 
+            this.removebtn.Location = new System.Drawing.Point(568, 414);
+            this.removebtn.Name = "removebtn";
+            this.removebtn.Size = new System.Drawing.Size(107, 23);
+            this.removebtn.TabIndex = 4;
+            this.removebtn.Text = "Remove last";
+            this.removebtn.UseVisualStyleBackColor = true;
+            this.removebtn.Click += new System.EventHandler(this.removebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removebtn);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button removebtn;
     }
 }
--- to-do list/to-do list/Form1.cs
+++ to-do list/to-do list/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,35 @@ namespace to_do_list
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = " ";
+            
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            TextWriter txt = new StreamWriter("C:\\users\\vasko\\desktop\\demo.txt");
+            foreach (var item in listBox1.Items)
+            {
+                txt.WriteLine(item.ToString());
+            }
+            txt.Close();
+            MessageBox.Show("File Saved");
+        }
+
+        private void removebtn_Click(object sender, EventArgs e)
+        {
+            int i = listBox1.Items.Count - 1;
+            listBox1.Items.RemoveAt(i);
+            i--;
+        }
     }
 }
